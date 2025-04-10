06acb70df8a4ff36e1d21a4528905be3248ebfeb
Abhinav Pandey <abhinavpandey1230@gmail.com>
2025-04-11 02:11:23 +0530

    Separate routes. Add test script. Add listDatabase function

--- database/database.go
+++ database/database.go
@@ -212,3 +212,26 @@ func (db *Database) Close() error {
 	}
 	return nil
 }
+
+func ListDatabases(root string) ([]string, error) {
+	entries, err := os.ReadDir(root)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []string{}, nil
+		}
+		return nil, err
+	}
+
+	var dbIDs []string
+	for _, e := range entries {
+		if !e.IsDir() {
+			continue
+		}
+
+		manifest := filepath.Join(root, e.Name(), "manifest.json")
+		if _, err := os.Stat(manifest); err == nil {
+			dbIDs = append(dbIDs, e.Name())
+		}
+	}
+	return dbIDs, nil
+}
