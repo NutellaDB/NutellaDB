833ca35844495eea85f885cda865ce1e5c767c4d
Varun-Kolanu <kolanuvarun739@gmail.com>
2025-04-11 06:31:35 +0530

    add cors

--- server/server.go
+++ server/server.go
@@ -5,11 +5,13 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/spf13/cobra"
 )
 
 func Server(cmd *cobra.Command) {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	routes.SetupRoutes(app)
 
